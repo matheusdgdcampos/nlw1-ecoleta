8b23f80e9c2b2e5d123c18ab7dc054f1519b2d29
Teuuz1994 <59451348+Teuuz1994@users.noreply.github.com>
2020-06-04 10:23:00 -0300

    feat: configuration of cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,12 @@
 import express from "express";
+import cors from "cors";
 import { resolve } from "path";
 import routes from "./routes";
 
 const app = express();
 const port = 3333;
 
+app.use(cors());
 app.use(express.json());
 //  rota para acessar todos os ícones cadastrados
 app.use("/uploads", express.static(resolve(__dirname, "..", "uploads")));
