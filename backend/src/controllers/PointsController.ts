d45229cdba83d77af44d96d38860152ad4eb4053
Teuuz1994 <59451348+Teuuz1994@users.noreply.github.com>
2020-06-04 10:20:47 -0300

    feat: finaly routes of points of collect

--- backend/src/controllers/PointsController.ts
+++ backend/src/controllers/PointsController.ts
@@ -2,7 +2,44 @@ import { Request, Response } from "express";
 import { connection } from "../database/connection";
 
 export default class PointsController {
-  async store(req: Request, res: Response) {
+  async index(req: Request, res: Response) {
+    const { city, uf, items } = req.query;
+
+    const parsedItems = String(items)
+      .split(",")
+      .map((item) => Number(item.trim()));
+
+    const points = await connection("points")
+      .join("point_items", "points.id", "=", "point_items.point_id")
+      .whereIn("point_items.item_id", parsedItems)
+      .where("city", String(city))
+      .where("uf", String(uf))
+      .distinct()
+      .select("points.*");
+
+    return res.json(points);
+  }
+
+  async show(req: Request, res: Response) {
+    const { id } = req.params;
+
+    const point = await connection("points").where("id", id).first();
+
+    if (!point) return res.status(400).json({ message: "Point not found." });
+
+    /**
+     * listando todos os items junto com o ponto
+     * de coleta.
+     */
+    const items = await connection("items")
+      .join("point_items", "items.id", "=", "point_items.item_id")
+      .where("point_items.point_id", id)
+      .select("items.title");
+
+    return res.json({ point, items });
+  }
+
+  async create(req: Request, res: Response) {
     const {
       name,
       email,
@@ -14,7 +51,9 @@ export default class PointsController {
       items,
     } = req.body;
 
-    await connection("points").insert({
+    const trx = await connection.transaction(); // MONITORA ERROS DE TRANSAÇÕES DO KNEX
+
+    const point = {
       image: "image-fake",
       name,
       email,
@@ -23,8 +62,27 @@ export default class PointsController {
       longitude,
       city,
       uf,
+    };
+
+    const insertedIds = await trx("points").insert(point);
+
+    const point_id = insertedIds[0];
+
+    const pointItems = items.map((item_id: number) => {
+      return {
+        item_id,
+        point_id,
+      };
     });
 
-    return res.json({ success: true });
+    await trx("point_items").insert(pointItems);
+
+    // Insere os dados na tabela após definir a transation.
+    await trx.commit();
+
+    return res.json({
+      id: point_id,
+      ...point,
+    });
   }
 }
--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -10,6 +10,9 @@ const pointsController = new PointsController();
 
 //  ROTAS
 routes.get("/items", itemsController.index);
-routes.post("/points", pointsController.store);
+
+routes.post("/points", pointsController.create);
+routes.get("/points/:id", pointsController.show);
+routes.get("/points", pointsController.index);
 
 export default routes;
